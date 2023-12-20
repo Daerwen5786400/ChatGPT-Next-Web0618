1a50667426070eb7e898ae479aa48c157d64da5a
Daerwen5786400 <131731573+Daerwen5786400@users.noreply.github.com>
2023-12-20 16:41:20 +0800

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,7 +7,7 @@ import { type Metadata } from "next";
 
 export const metadata: Metadata = {
   title: "Metant Chat",
-  description: "Your personal ChatGPT Chat Bot.",
+  description: "你的AI问答小精灵.",
   viewport: {
     width: "device-width",
     initialScale: 1,
@@ -18,7 +18,7 @@ export const metadata: Metadata = {
     { media: "(prefers-color-scheme: dark)", color: "#151515" },
   ],
   appleWebApp: {
-    title: "ChatGPT Next Web",
+    title: "Metant Chat",
     statusBarStyle: "default",
   },
 };
